8a0e89618e1bff4ee3a2eeb3c7e43eb5c6b294cb
Michael Schneeberger <michael.schneeb@gmail.com>
2025-08-14 14:24:54 +0200

    Improve safety filter implementation

--- matlab/SafetyFilterSystemDPC.m
+++ matlab/SafetyFilterSystemDPC.m
@@ -11,22 +11,24 @@ classdef SafetyFilterSystemDPC < matlab.System
         gamma_B_data
         r
         l
-        init_vpcc_f
+        init_v_pcc_f = [1; 0];
     end
 
     % Pre-computed constants or internal states
     properties (Access = private)
-        init_m = [0; 0];
-        init_dv_pcc_f = [0; 0];
+        init_dm = [0; 0];
+        % init_v_pcc_f = [1; 0];
         init_dm_corr = [0; 0];
+        init_dv_corr = [0; 0];
         init_active = 0;
         init_lambda = [0; 0];
     end
 
     properties(DiscreteState)
-        m
-        dv_pcc_f
+        dm
+        v_pcc_f
         dm_corr
+        dv_corr
         active
         lambda
     end
@@ -34,57 +36,58 @@ classdef SafetyFilterSystemDPC < matlab.System
     methods (Access = protected)
 
         function setupImpl(obj)
-            obj.m = obj.init_m;
-            obj.dv_pcc_f = obj.init_dv_pcc_f;
+            obj.dm = obj.init_dm;
+            obj.v_pcc_f = obj.init_v_pcc_f;
             obj.dm_corr = obj.init_dm_corr;
+            obj.dv_corr = obj.init_dv_corr;
             obj.active = obj.init_active;
             obj.lambda = obj.init_lambda;
         end
 
         function resetImpl(obj)
-            obj.m = obj.init_m;
-            obj.dv_pcc_f = obj.init_dv_pcc_f;
+            obj.dm = obj.init_dm;
+            obj.v_pcc_f = obj.init_v_pcc_f;
             obj.dm_corr = obj.init_dm_corr;
+            obj.dv_corr = obj.init_dv_corr;
             obj.active = obj.init_active;
             obj.lambda = obj.init_lambda;
         end
 
-        function [m, v_pcc_f, dm_corr, active, lambda] = outputImpl(obj, ~)
-            m = obj.m;
-            v_pcc_f = obj.dv_pcc_f + [1; 0];
+        function [dm, v_pcc_f, dm_corr, dv_corr, active, lambda] = outputImpl(obj, ~)
+            dm = obj.dm;
+            v_pcc_f = obj.v_pcc_f;
             dm_corr = obj.dm_corr;
+            dv_corr = obj.dv_corr;
             active = obj.active;
             lambda = obj.lambda;
         end
 
         function updateImpl(obj, u)
 
-            % read arguments
-            ff = u(1:2);
+            % read input arguments
+            dm_ref = u(1:2);
             i_ref = u(3:4);
             i = u(5:6);
             v_pcc = u(7:8);
-            w = u(9);
-            enable = u(10);
-
-            dv_pcc = v_pcc - [1; 0];
-
-            % set state default values
-            obj.dm_corr = [0; 0];
-            obj.lambda = [0; 0];
+            enable = u(9);
 
             if ~enable
 
                 obj.active = 0;
 
-                obj.m = ff + obj.init_vpcc_f;
-                obj.dv_pcc_f = obj.init_vpcc_f - [1; 0];
+                obj.dm = dm_ref;
+                obj.v_pcc_f = obj.init_v_pcc_f;
+
+                obj.dm_corr = [0; 0];
+                obj.dv_corr = [0; 0];
+                obj.lambda = [0; 0];
 
                 return
 
             end
                 
-            x = [i; obj.dv_pcc_f; i_ref; dv_pcc];
+            dv_pcc_f = obj.v_pcc_f - v_pcc;
+            x = [i; dv_pcc_f; i_ref];
 
             xx = reshape(x * x', [], 1);
 
@@ -95,23 +98,16 @@ classdef SafetyFilterSystemDPC < matlab.System
             V = obj.V_data * x012;
             B = obj.B_data * x012;
 
-            V2 = 0.1 * V + 0.9 * B;
-
-            J = [0 -1; 1 0];
-            cross_term = (obj.r + J*w*obj.l) * i;
-
             obj.active = 1;
 
             u_ref = [
-                % desired voltage applied to the converter (without
-                % cross-terms) relative to v_c = [1; 0]
-                ff - cross_term + obj.dv_pcc_f;
-
-                100 * (dv_pcc - obj.dv_pcc_f)
+                dm_ref + dv_pcc_f;
+                -100 * dv_pcc_f
+                % -10 * dv_pcc_f
             ];
 
             f_sys = obj.f_data * x;
-            G_sys = reshape(obj.G_data, 8, 4);
+            G_sys = reshape(obj.G_data, 6, 4);
 
             nV = obj.nV_data * x01;
             nB = obj.nB_data * x01;
@@ -130,29 +126,35 @@ classdef SafetyFilterSystemDPC < matlab.System
                 ];       
             b = [ ...
                    -nB' * f_sys - gamma_B * B;
-                   -nV' * f_sys - gamma_V * V ...
+                   (-nV' * f_sys - gamma_V * V) ...
                  ];
 
-            lb = [-2.3; -1.3; -500; -500];
-            ub = [0.3; 1.3; 500; 500];
+            lb = [-2; -2; -500; -500];
+            ub = [2; 2; 500; 500];
 
             x0 = u_ref;
 
             options = optimoptions('quadprog', 'Algorithm', 'active-set');
             [u, ~, ~, ~, lambda_val] = quadprog(H, f, A, b, [], [], lb, ub, x0, options);
     
-            obj.lambda = lambda_val.ineqlin(1:2,1);
+            % next_dv_pcc_f = dv_pcc_f + obj.ts * u(3:4, 1);
+            next_dv_pcc_f = dv_pcc_f + obj.ts * u_ref(3:4, 1);
+
+            % update states
+            obj.v_pcc_f = v_pcc + next_dv_pcc_f;
+            % obj.dm = u(1:2, 1) - next_dv_pcc_f;
+            obj.dm = u(1:2, 1) - dv_pcc_f;
+
+            % debugging
             obj.dm_corr = u(1:2, 1) - u_ref(1:2, 1);
-
-            obj.m = u(1:2, 1) + cross_term + [1; 0];
-
-            obj.dv_pcc_f = obj.dv_pcc_f + obj.ts * u(3:4, 1);
+            obj.dv_corr = u(3:4, 1) - u_ref(3:4, 1);
+            obj.lambda = lambda_val.ineqlin(1:2,1);
 
         end
 
-        function flag = isInputDirectFeedthroughImpl(~)
-            flag = false;
-        end
+        % function flag = isInputDirectFeedthroughImpl(~)
+        %     flag = true;
+        % end
 
     end
 end
